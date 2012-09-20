8c99219aa41de8767d4fd1e699d198365fc91ba3
Marc Friederich <marc@antistatique.net>
2012-09-20 21:17:16 +0200

    ajout du clear/cache

--- src/controllers.php
+++ src/controllers.php
@@ -56,6 +56,26 @@ $app->get('/{_locale}/last', function () use ($app) {
 ->assert('_locale', $localeRegExp);
 ;
 
+$app->get('cache/clear', function () use ($app) {
+
+    $cacheDir = $app['twig']->getCache();
+    $finder = new Symfony\Component\Finder\Finder();
+
+    foreach($finder->in($cacheDir)->files() as $file) {
+        print "- ".$file->getRealpath()."<br>\n";
+        unlink($file->getRealpath());
+    }
+
+    foreach($finder->in($cacheDir)->directories() as $file) {
+        //print "- ".$file->getRealpath()."<br>\n";
+        //rmdir($file->getRealpath());
+    }
+    
+    $response = 'cache:clear';
+
+    return $response;
+})
+->bind('cache_clear');
 
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
