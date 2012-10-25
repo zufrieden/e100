4946fd379ffef2624ee9f180bfcc4716665425d7
Renato Kempter <renato.kempter@gmail.com>
2012-10-25 11:49:12 +0200

    Show the current state of goals. Mark as read or not read

---
+++ src/E100/CoreBundle/Controller/GoalController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace E100\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class GoalController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template("E100CoreBundle:Goal:index.html.twig")
+     */
+    public function indexAction($id)
+    {
+
+    	$repository = $this->getDoctrine()->getRepository('E100CoreBundle:Text');
+    	$text = $repository->findOneBy(array('id' => $id));
+        
+        return array('text' => $text);
+    }
+
+    public function markRead($id)
+    {
+    	$today = date("Y-m-d");
+    	$readText = new ReadText();
+    	$readText->setText($id);
+
+    	// Set user from current session
+    	$readText->setUser(1);
+    	$readText->setDate($today);
+
+    	$em = $this->getDoctrine()->getManager();
+    	$em->persist($product);
+    	$em->flush();
+    }
+
+    public function markNotRead($id)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$repository = $em->getRepository('E100CoreBundle:ReadText');
+
+    	$readText = $repository->findOneBy(array('user' => $userid, 'text' => $id));
+
+    	if($readText) {
+    		$repository->remove($readText);
+    		$em->flush();
+    	}
+    }
+}
