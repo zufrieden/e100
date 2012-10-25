b88229e4b77509498df17593e7526d8e798a8409
Renato Kempter <renato.kempter@gmail.com>
2012-10-25 11:49:23 +0200

    Mark as favorite

--- src/E100/CoreBundle/Controller/FavoritesController.php
+++ src/E100/CoreBundle/Controller/FavoritesController.php
@@ -11,10 +11,46 @@ class FavoritesController extends Controller
 {
     /**
      * @Route("/", name="favorites")
-     * @Template()
+     * @Template("E100CoreBundle:Favorites:favorites.html.twig")
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('E100CoreBundle:Default:index.html.twig', array('name' => 'default'));
+    	// User from session
+
+    	$repositoryUser = $this->getDoctrine()->getRepository('E100CoreBundle:User');
+    	$user = $repositoryUser->findOneById(1);
+
+    	$favorites = $user->getFavorites();
+
+        return array('favorites' => $favorites);
+    }
+
+	/**
+     * @Route("/add/{id}", name="addFavorites")
+     */
+    public function addAction($id)
+    {
+
+    	$repository = $this->getDoctrine()->getRepository('E100CoreBundle:Text');
+    	$text = $repository->findOneBy(array('id' => $id));
+    	
+    	// User from user session
+    	$repositoryUser = $this->getDoctrine()->getRepository('E100CoreBundle:User');
+    	$user = $repositoryUser->findOneById(1);
+    	$user->addFavorite($text);	
+    }
+
+    /**
+     * @Route("/delete/{id}", name="addFavorites")
+     */
+    public function deleteAction($id)
+    {
+    	$repositoryText = $this->getDoctrine()->getRepository('E100CoreBundle:Text');
+    	$text = $repository->findOneById($id);
+
+    	// User from user session
+    	$repositoryUser = $this->getDoctrine()->getRepository('E100CoreBundle:User');
+    	$user = $repositoryUser->findOneById(1);
+    	$user->removeFavorite($text);
     }
 }
