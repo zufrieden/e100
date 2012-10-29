0f83de79e8d1a8a4659d598ba09219a4e51ee5de
Renato Kempter <renato.kempter@gmail.com>
2012-10-29 12:18:44 +0100

    Create form for new note

--- src/E100/CoreBundle/Controller/NotesController.php
+++ src/E100/CoreBundle/Controller/NotesController.php
@@ -5,14 +5,16 @@ namespace E100\CoreBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use E100\CoreBundle\Entity\Note;
+
 
 class NotesController extends Controller
 {
     /**
      * @Route("/}", name="notes")
-     * @Template("E100CoreBundle:Default:index.html.twig")
+     * @Template("E100CoreBundle:Notes:notes.html.twig")
      */
-    public function indexAction($id)
+    public function indexAction()
     {
         return array('name' => 'default');
     }
@@ -23,8 +25,8 @@ class NotesController extends Controller
     public function deleteAction($id)
     {
     	// Get User form Session
-    	$repository = $this->getDoctrine()->getRepository('E100CoreBundle:User');
-    	$user = $repository->findOneById(1);
+    	$user = $this->getUser();
+        $id = $user->getId();
 
     	$user->removeNote($id);
     	$this->getDoctrine()->flush();
@@ -34,10 +36,28 @@ class NotesController extends Controller
      * @Route("/edit/{id}", name="editNote")
      * @Template("E100CoreBundle:Default:index.html.twig")
      */
-    public function editAction()
+    public function editAction($id)
     {
-    	// Update
+    	;// Update
     }
 
+    /**
+     * @Route("/new/{id}", name="newNote")
+     */
+    public function newAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository('E100CoreBundle:Text');
+        $text = $repository->findOneBy(array('id' => $id));
 
+        $note = new Note();
+        $note->setText($text);
+
+        $form = $this->createFormBuilder($note)
+                        ->add('note_text', 'textarea')
+                        ->getForm();
+
+        return $this->render('E100CoreBundle:Notes:new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
 }
