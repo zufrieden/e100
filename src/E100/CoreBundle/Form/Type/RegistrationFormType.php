c02535a32329f32bdef91ef105b8c92a74917422
Gilles Doge <gilles@antistatique.net>
2013-03-11 17:32:12 +0100

    Fix #17 Add language field in the Registration form

---
+++ src/E100/CoreBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace E100\CoreBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class RegistrationFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        // add your custom field
+        $builder->add('language', 'choice', array(
+            'choices' => array(
+                'fr' => 'French',
+                'en' => 'English',
+            )
+        ));
+    }
+
+    public function getName()
+    {
+        return 'e100_user_registration';
+    }
+}
