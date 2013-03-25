2fd2e3c4da079e42d1adeb3937c3cef0484f47e0
Renato Kempter <renato.kempter@gmail.com>
2013-03-25 11:43:37 +0100

    #22 show category progression

--- src/E100/CoreBundle/Controller/CategoriesController.php
+++ src/E100/CoreBundle/Controller/CategoriesController.php
@@ -16,9 +16,29 @@ class CategoriesController extends Controller
     {	
     	$repository = $this->getDoctrine()->getRepository('E100CoreBundle:Theme');
     	$newTestamCategories = $repository->findBy(array('testament' => 'new'));
-    	$oldTestamCategories = $repository->findBy(array('testament' => 'old'));;
+    	$oldTestamCategories = $repository->findBy(array('testament' => 'old'));
+
+        $user = $this->getUser();
+        $userId = $user->getId();
+
+        $query = $this->getDoctrine()->getEntityManager()->createQuery("
+            SELECT DISTINCT theme.id, COUNT(text.id) AS counter
+            FROM 
+                E100\CoreBundle\Entity\Text AS text,
+                E100\CoreBundle\Entity\Theme AS theme INDEX BY theme.id,
+                E100\CoreBundle\Entity\ReadText AS readtext
+            WHERE
+                text.theme = theme.id AND
+                text.id = readtext.text AND
+                readtext.user = ?1
+            GROUP BY theme.id
+        ");
+
+        $query->setParameter(1, $userId);
+        $result = $query->getResult();
         
         return array('newTestamentCategories' => $newTestamCategories,
-        			 'oldTestamentCategories' => $oldTestamCategories);
+        			 'oldTestamentCategories' => $oldTestamCategories,
+                     'categoryCount' => $result);
     }
 }
