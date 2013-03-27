d64babd2a2a3d81c2d616ddfadb27e629abf8101
Marc Friederich <marc@antistatique.net>
2013-03-27 14:29:23 +0100

    fix #35 textes motication

--- src/E100/CoreBundle/Controller/UserController.php
+++ src/E100/CoreBundle/Controller/UserController.php
@@ -30,22 +30,37 @@ class UserController extends Controller
         // Display message
         switch($numberOfTextReaded) {
             case 1:
-                $message = $this->get('translator')->trans('motivation.message1');
+                $message = $this->get('translator')->trans('motivation.cas1');
+                break;
+            case 3:
+                $message = $this->get('translator')->trans('motivation.cas3');
                 break;
             case 5:
-                $message = $this->get('translator')->trans('motivation.message2');
+                $message = $this->get('translator')->trans('motivation.cas5');
                 break;
             case 10:
-                $message = $this->get('translator')->trans('motivation.message3');
+                $message = $this->get('translator')->trans('motivation.cas10');
                 break;
-            case 25:
-                $message = $this->get('translator')->trans('motivation.message4');
+            case 20:
+                $message = $this->get('translator')->trans('motivation.cas20');
+                break;
+            case 33:
+                $message = $this->get('translator')->trans('motivation.cas33');
                 break;
             case 50:
-                $message = $this->get('translator')->trans('motivation.message5');
+                $message = $this->get('translator')->trans('motivation.cas50');
+                break;
+            case 90:
+                $message = $this->get('translator')->trans('motivation.cas90');
+                break;
+            case 95:
+                $message = $this->get('translator')->trans('motivation.cas95');
                 break;
             case 99:
-                $message = $this->get('translator')->trans('motivation.message6');
+                $message = $this->get('translator')->trans('motivation.cas99');
+                break;
+            case 100:
+                $message = $this->get('translator')->trans('motivation.cas100');
                 break;
         }
 
